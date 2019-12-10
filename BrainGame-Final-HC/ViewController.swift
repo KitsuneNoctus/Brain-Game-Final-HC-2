6c69fc0e13f7b2045739e78cb6ed8beb593d835f
KitsuneNoctus <theceintiant@gmail.com>
2019-12-10 14:32:58 -0800

    Adding buttons and stuff.

--- BrainGame-Final-HC/ViewController.swift
+++ BrainGame-Final-HC/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var easyButton: UIButton!
+    @IBOutlet weak var moderateButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    @IBOutlet weak var gameTitle: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func easyButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func moderateButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func hardButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
